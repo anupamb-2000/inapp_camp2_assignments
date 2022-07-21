9a3b31a94183a7d8133ce668a67df88dc5cbaefe
anupam <anupam.b@inapp.com>
2022-07-21 17:18:59 +0530

    CMS.py initial commit

---
+++ Python/Class Assignment/CMS.py
@@ -0,0 +1,16 @@
+#Write a program to build a simple Clinic Management System using Python
+#which can perform following operations inside class CMS
+
+#Admit : takes patient details frommthe user like name, gender, age, dob
+#blood group. A patient_id should be automatically generated
+
+#listPatients : displays the details of every patient
+
+#search : search using patient_id
+
+#Delete : delete using patient_id
+
+#Update : will ask for patient_id and other details. It will replace the other details with new details.
+
+#Create inherited classes "OP" and "NonOP". "OP" should have the additional Admit function and NonOP a function to generate an OP Ticket
+#with incrementing no. for every patient.
