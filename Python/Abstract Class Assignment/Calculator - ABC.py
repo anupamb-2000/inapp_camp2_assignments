9c7ae48028e53c396ee55ed11030eec626c40069
anupam <anupam.b@inapp.com>
2022-07-25 13:53:11 +0530

    Added Abstract Class Assignment

---
+++ Python/Abstract Class Assignment/Calculator - ABC.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+#Defining the class
+class Calculator(ABC):
+
+    @abstractmethod
+    def calculator(self):
+        pass
+
+    @property
+    def operandA(self): 
+        return self.__num1
+
+    @operandA.setter
+    def operandA(self, num):
+        if(isinstance(num, int)):
+            self.__num1 = num
+        else:
+            raise ValueError(f"Invalid operand(A)!")
+
+    @property
+    def operandB(self): 
+        return self.__num2
+
+    @operandB.setter
+    def operandB(self, num):
+        if(isinstance(num, int)):
+            self.__num2 = num
+        else:
+            raise ValueError(f"Invalid operand(B)!")
+
+class CalcSum(Calculator):
+    def calculator(self):
+        print(f"Addition result : {self.operandA + self.operandB}")
+
+class CalcDiff(Calculator):
+    def calculator(self):
+        print(f"Subtraction result : {self.operandA - self.operandB}")
+
+class CalcProd(Calculator):
+    def calculator(self):
+        print(f"Multiplication result : {self.operandA * self.operandB}")
+
+class CalcQuo(Calculator):
+    def calculator(self):
+        print(f"Division result : {self.operandA + self.operandB}")
+
+#creating the object
+add = CalcSum()
+sub = CalcDiff()
+mul = CalcProd()
+div = CalcQuo()
+#calling the methods
+for operation in [add, sub, mul, div]:
+    operation.operandA = 10
+    operation.operandB = 5
+    operation.calculator()
